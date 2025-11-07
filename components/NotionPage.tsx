1b11cf9e35ccb4637b86a1d7aa3834b9832c21b2
Luyue-ye <139524990+Luyue-ye@users.noreply.github.com>
2025-11-07 01:58:36 -0500

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,1 +1,172 @@
+import cs from 'classnames'
+import dynamic from 'next/dynamic'
+import Image from 'next/legacy/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { type PageBlock } from 'notion-types'
+import { getBlockTitle, getPageProperty } from 'notion-utils'
+import * as React from 'react'
+import BodyClassName from 'react-body-classname'
+import { type NotionComponents, NotionRenderer } from 'react-notion-x'
+import { useSearchParam } from 'react-use'
+
+import type * as types from '@/lib/types'
+import * as config from '@/lib/config'
+import { mapImageUrl } from '@/lib/map-image-url'
+import { getCanonicalPageUrl, mapPageUrl } from '@/lib/map-page-url'
+import { searchNotion } from '@/lib/search-notion'
+import { useDarkMode } from '@/lib/use-dark-mode'
+
+import { Footer } from './Footer'
+import { GitHubShareButton } from './GitHubShareButton'
+import { Loading } from './Loading'
+import { NotionPageHeader } from './NotionPageHeader'
+import { Page404 } from './Page404'
+import { PageHead } from './PageHead'
+import ClientToc from './ClientToc'
+import styles from './styles.module.css'
+
+// optional components
+const Code = dynamic(() =>
+  import('react-notion-x/build/third-party/code').then((m) => m.Code)
+)
+const Collection = dynamic(() =>
+  import('react-notion-x/build/third-party/collection').then((m) => m.Collection)
+)
+const Equation = dynamic(() =>
+  import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
+)
+const Pdf = dynamic(
+  () => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf),
+  { ssr: false }
+)
+const Modal = dynamic(
+  () =>
+    import('react-notion-x/build/third-party/modal').then((m) => {
+      m.Modal.setAppElement('.notion-viewport')
+      return m.Modal
+    }),
+  { ssr: false }
+)
+
+// react-tweet
+import { Tweet as XTweet, TweetSkeleton } from 'react-tweet'
+function Tweet({ id }: { id: string }) {
+  return (
+    <React.Suspense fallback={<TweetSkeleton />}>
+      <XTweet id={id} />
+    </React.Suspense>
+  )
+}
+
+export function NotionPage({
+  site,
+  recordMap,
+  error,
+  pageId
+}: types.PageProps) {
+  const router = useRouter()
+  const lite = useSearchParam('lite')
+  const { isDarkMode } = useDarkMode()
+  const isLiteMode = lite === 'true'
+
+  const components = React.useMemo<Partial<NotionComponents>>(
+    () => ({
+      nextLegacyImage: Image,
+      nextLink: Link,
+      Code,
+      Collection,
+      Equation,
+      Pdf,
+      Modal,
+      Header: NotionPageHeader,
+      Tweet
+    }),
+    []
+  )
+
+  const siteMapPageUrl = React.useMemo(() => {
+    const params: Record<string, string> = {}
+    if (lite) params.lite = lite
+    const searchParams = new URLSearchParams(params)
+    return site ? mapPageUrl(site, recordMap!, searchParams) : undefined
+  }, [site, recordMap, lite])
+
+  // 下面才做条件分支，所有 Hook 已在上面执行
+  const keys = Object.keys(recordMap?.block || {})
+  const block = recordMap?.block?.[keys[0]!]?.value
+
+  if (router.isFallback) return <Loading />
+  if (error || !site || !block) {
+    return <Page404 site={site} pageId={pageId} error={error} />
+  }
+
+  const title = getBlockTitle(block, recordMap) || site.name
+
+  const canonicalPageUrl = config.isDev
+    ? undefined
+    : getCanonicalPageUrl(site, recordMap)(pageId)
+
+  const socialImage = mapImageUrl(
+    getPageProperty<string>('Social Image', block, recordMap) ||
+      (block as PageBlock).format?.page_cover ||
+      config.defaultPageCover,
+    block
+  )
+
+  const socialDescription =
+    getPageProperty<string>('Description', block, recordMap) ||
+    config.description
+
+  const footer = React.useMemo(() => <Footer />, [])
+
+  return (
+    <>
+      <PageHead
+        pageId={pageId}
+        site={site}
+        title={title}
+        description={socialDescription}
+        image={socialImage}
+        url={canonicalPageUrl}
+        isBlogPost={false}
+      />
+
+      {/* 固定右侧目录 */}
+      <ClientToc />
+
+      {isLiteMode && <BodyClassName className='notion-lite' />}
+      {isDarkMode && <BodyClassName className='dark-mode' />}
+
+      <div className="notion-page-wrapper">
+        <main className="notion-content">
+          <NotionRenderer
+            bodyClassName={cs(
+              styles.notion,
+              pageId === site.rootNotionPageId && 'index-page'
+            )}
+            darkMode={isDarkMode}
+            components={components}
+            recordMap={recordMap}
+            rootPageId={site.rootNotionPageId}
+            rootDomain={site.domain}
+            fullPage={!isLiteMode}
+            previewImages={!!recordMap.preview_images}
+            showCollectionViewDropdown={false}
+            showTableOfContents={false} // 关闭内置目录
+            defaultPageIcon={config.defaultPageIcon}
+            defaultPageCover={config.defaultPageCover}
+            defaultPageCoverPosition={config.defaultPageCoverPosition}
+            mapPageUrl={siteMapPageUrl}
+            mapImageUrl={mapImageUrl}
+            searchNotion={config.isSearchEnabled ? searchNotion : undefined}
+            footer={footer}
+          />
+        </main>
+      </div>
+
+      <GitHubShareButton />
+    </>
+  )
+}
 
